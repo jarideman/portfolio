3a3752fd80de9f934bd93850f5f16ce82a2e2377
Jari <jarivandenboogert@gmail.com>
2024-03-07 20:15:23 +0100

    Fix bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Laravel</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 
